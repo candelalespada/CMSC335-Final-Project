f32976800d76f74b539823056cf8e0d1adf130b3
Kate Granelli <kategranelli@Kates-MacBook-Pro-107.local>
2025-12-11 17:03:00 -0500

    hi

--- server.js
+++ server.js
@@ -45,7 +45,8 @@ mongoose
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(__dirname));
 
 // ROUTES
 const recipeRoutes = require("./routes/recipes");
